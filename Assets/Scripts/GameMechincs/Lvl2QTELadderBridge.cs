d1f10026c45b29a56b9c3a67b8c67043875a0bdf
BunBao <135566105+BunBaoi@users.noreply.github.com>
2024-10-22 04:49:37 +1100

    QTE error not functioning

--- Assets/Scripts/GameMechincs/Lvl2QTELadderBridge.cs
+++ Assets/Scripts/GameMechincs/Lvl2QTELadderBridge.cs
@@ -1,23 +1,144 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lvl2QTELadderBridge : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PlayerStats playerStats;
+
+
+    public GameObject PassBox;
+    public GameObject DisplayKey;
+
+    public int QTEGen;
+    public int WaitingKey;
+    public int CorrectKey;
+    public int CountingDown;
+
+    // Timer
+    public float CountDownTimer = 100;
+    public int MaxTimer;
+
     void Start()
     {
-        
+        playerStats = GetComponent<PlayerStats>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (WaitingKey == 0)
+        {
+            QTEGen = Random.Range(1, 3);
+            CountingDown = 1;
+            StopAllCoroutines();
+            StartCoroutine(CountDown());
+            if (QTEGen == 1)
+            {
+                WaitingKey = 1;
+                DisplayKey.GetComponent<Text>().text = "A";
+            }
+            if (QTEGen == 2)
+            {
+                WaitingKey = 1;
+                DisplayKey.GetComponent<Text>().text = "D";
+            }
+        }
+
+        if (QTEGen == 1)
+        {
+            if (Input.anyKeyDown)
+            {
+                if(Input.GetButtonDown("A"))
+                {
+                    CorrectKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    CorrectKey = 2;
+                    StartCoroutine(KeyPressing());
+                    
+                }
+            }
+        }
+        if (QTEGen == 2)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetButtonDown("D"))
+                {
+                    CorrectKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    CorrectKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+
+        IEnumerator KeyPressing()
+        {
+            QTEGen = 3;
+            if (CorrectKey == 1)
+            {
+                CountingDown = 2;
+                PassBox.GetComponent<TextMesh>().text = "Pass";
+                yield return new WaitForSeconds(1.5f);
+                CorrectKey = 0;
+                PassBox.GetComponent<TextMesh>().text = "";
+                DisplayKey.GetComponent<TextMesh>().text = "";
+                yield return new WaitForSeconds(1.5f);
+                WaitingKey = 0;
+                CountingDown = 1;
+            }
+            if (CorrectKey == 2)
+            {
+                CountingDown = 2;
+                PassBox.GetComponent<TextMesh>().text = "Failed";
+                yield return new WaitForSeconds(1.5f);
+                CorrectKey = 0;
+                PassBox.GetComponent<TextMesh>().text = "";
+                DisplayKey.GetComponent<TextMesh>().text = "";
+                yield return new WaitForSeconds(1.5f);
+                WaitingKey = 0;
+                CountingDown = 1;
+            }
+        }
+
+        IEnumerator CountDown()
+        {
+            yield return new WaitForSeconds(3.5f);
+            if (CountingDown == 1)
+            {
+                QTEGen = 4;
+                CountingDown = 2;
+                PassBox.GetComponent<TextMesh>().text = "Failed";
+                yield return new WaitForSeconds(1.5f);
+                CorrectKey = 0;
+                PassBox.GetComponent<TextMesh>().text = "";
+                DisplayKey.GetComponent<TextMesh>().text = "";
+                yield return new WaitForSeconds(1.5f);
+                WaitingKey = 0;
+                CountingDown = 1;
+            }
+        }
+
+        if (CountDownTimer >= 0)
+        {
+            updateTimer();
+        }
     }
 
-    public void QTE()
+    public void updateTimer()
     {
-
+        CountDownTimer -= Time.deltaTime;
+        if (CountDownTimer <= 0)
+        {
+            playerStats.IsAlive = false;
+            Debug.Log("Player Dead");
+        }
     }
 }
