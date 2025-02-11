37f89f3bbfa7a5545a400e8a6401967db24b4e66
OCEZkw <133872998+OCEZkw@users.noreply.github.com>
2025-02-11 23:11:31 +1100

    Created Cutscene for onboarding

---
+++ Assets/Scripts/CutSceneScript/ActivateCutScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivateCutScene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector playableDirector;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playableDirector.Play();
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
