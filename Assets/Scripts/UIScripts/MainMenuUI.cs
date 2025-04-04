392db71ff17a2bd56685b87a0fe8bca4faec8468
BunBao <135566105+BunBaoi@users.noreply.github.com>
2025-04-05 06:44:40 +1100

    Main Menu & Zoom fix
    - Main menu
     - Load new scene
     - Setting, credits, quit buttons work

---
+++ Assets/Scripts/UIScripts/MainMenuUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public static ScriptableObject playerSettings;
+
+    public void Continue()
+    {
+
+    }
+
+    public void NewGame()
+    {
+
+        SceneManager.LoadScene("Level3");
+        //SceneManager.LoadScene("Testing Level"); // Just for Testing
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+}
